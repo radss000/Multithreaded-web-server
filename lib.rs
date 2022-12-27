fe40d92ea295f8db3a9a5a4bccc5a656535f40e9
rad$$ <105228117+radss000@users.noreply.github.com>
2022-12-27 12:13:52 +0100

    Create lib.rs

---
+++ lib.rs
@@ -0,0 +1,127 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+};
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Self { id, thread }
+    }
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            // create some threads and store them in the vector
+            workers.push(Worker::new(id));
+        }
+        Self { workers }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        // Find a worker thread that is not busy and execute the given closure on it
+        for worker in &self.workers {
+            if let Ok(thread) = worker.thread.try_join() {
+                worker.thread = thread::spawn(f);
+                return;
+            }
+        }
+       
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+};
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Self { id, thread }
+    }
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            // create some threads and store them in the vector
+            workers.push(Worker::new(id));
+        }
+        Self { workers }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        // Find a worker thread that is not busy and execute the given closure on it
+        for worker in &self.workers {
+            if let Ok(thread) = worker.thread.try_join() {
+                worker.thread = thread::spawn(f);
+                return;
+            }
+        }
+           // If all worker threads are busy, execute the closure on the current thread
+        f();
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4); // Create a thread pool with 4 worker threads
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
+    
